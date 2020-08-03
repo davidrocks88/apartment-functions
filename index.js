9c88cfcab3b8e8ead03578482e4b39f901492f59
David Bernstein <davidbernstein@David-Bernsteins-MacBook-Pro.local>
2020-08-03 18:57:19 -0400

    Fixed api key

--- index.js
+++ index.js
@@ -7,7 +7,7 @@ const serviceAccount = {
   type: process.env.SERVICE_ACCOUNT_type,
   project_id: process.env.SERVICE_ACCOUNT_project_id,
   private_key_id: process.env.SERVICE_ACCOUNT_private_key_id,
-  private_key: process.env.SERVICE_ACCOUNT_private_key,
+  private_key: process.env.SERVICE_ACCOUNT_private_key.replace(/\\n/g, '\n'),
   client_email: process.env.SERVICE_ACCOUNT_client_email,
   client_id: process.env.SERVICE_ACCOUNT_client_id,
   auth_uri: process.env.SERVICE_ACCOUNT_auth_uri,
